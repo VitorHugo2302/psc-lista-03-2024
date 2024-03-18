859b42947e20256f9458ac7e2b79a2a7627bd398
VitorHugo2302 <vitorhugos165@gmail.com>
2024-03-17 22:53:36 -0300

    Update Exercicio6.java

--- Exercicio6.java
+++ Exercicio6.java
@@ -3,27 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package lista3;
-
 import java.util.Scanner;
 import java.util.Random;
-
 /**
  *
  * @author Vitor Hugo
  */
 public class Exercicio6 {
-
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-
+        
         System.out.print("Digite o primeiro número inteiro: ");
         int numero1 = entrada.nextInt();
-
+        
         System.out.print("Digite o segundo número inteiro: ");
         int numero2 = entrada.nextInt();
-
+        
         int menorNumero, maiorNumero;
-
+        
         if (numero1 < numero2) {
             menorNumero = numero1;
             maiorNumero = numero2;
@@ -31,18 +28,18 @@ public class Exercicio6 {
             menorNumero = numero2;
             maiorNumero = numero1;
         }
-
+        
         Random random = new Random();
         int numeroSorteado = random.nextInt(maiorNumero - menorNumero + 1) + menorNumero;
-
+        
         System.out.println("Número sorteado: " + numeroSorteado);
-
+        
         if (numeroSorteado % 2 == 0) {
             System.out.println("O número sorteado é par.");
         } else {
             System.out.println("O número sorteado é ímpar.");
         }
-
+        
         entrada.close();
     }
 }
